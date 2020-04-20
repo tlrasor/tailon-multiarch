e23d073225bfbb3a43676e3f2a3722d71e3dd46a
Travis Rasor <travis@thathanka.org>
2020-04-20 00:08:49 -0500

    build script

--- build.sh
+++ build.sh
@@ -3,10 +3,8 @@
 PLATFORMS="linux/amd64,linux/arm64,linux/arm/v7"
 GIT_TAG=`git log --pretty=format:'%h' -n 1`
 
-export TAILON_DOCKER_TAG=
-export TAILON_DOCKER_TAG_LATEST=
 docker buildx build -t"tlrasor/tailon:latest" \
                     -t "tlrasor/tailon:$GIT_TAG" \
-                    -f tailon.Dockerfile \
+                    -f Dockerfile \
                     --push --platform $PLATFORMS .
 
